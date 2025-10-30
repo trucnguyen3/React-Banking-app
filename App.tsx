9f897218f34bc04cd3c20721167ba29231f17ba9
Tr14 <xuantrucx111@gmail.com>
2025-10-30 15:27:41 +0700

    update deeplink

--- App.tsx
+++ App.tsx
@@ -230,60 +230,6 @@ const LoginScreen: React.FC<{ navigation: any }> = ({ navigation, onLogin }) => 
             };
 */
            useEffect(() => {
-             const handleDeepLink = (url) => {
-               console.log('Opened with URL:', url);
-                try {
-                    const parsed = new URL(url);
-                    const path = parsed.pathname.replace('/', '');
-                    const screenName = path ? `${path}` : null;
-
-                    const referralCodeOnDeeplink = parsed.searchParams.get('referral_code') || null;
-                    const referralUserIdOnDeeplink = parsed.searchParams.get('af_referrer_customer_id') || null;
-
-                    if (screenName) {
-                      console.log('Navigating to:', screenName);
-                      if (os === 'ios') {
-                          console.log('Running on iOS');
-                            fetch('https://script.google.com/macros/s/AKfycbwtc4Gn367FMyA4s3owITC0xagHqbymYWtf-CL_4A6X06PSW33lzehWRV4hy2s5xLg/exec', {
-                                method: 'POST',
-                                headers: { 'Content-Type': 'application/json' },
-                                body: JSON.stringify({
-                                    os: os,
-                                    af_method: "parsed.pathname.replace",
-                                    data: screenName,
-                                }),
-                            })
-                            .then(res => res.text())
-                            .then(console.log)
-                            .catch(console.error);
-                      } else if (os === 'android') {
-                        console.log('Running on Android');
-                        fetch('https://script.google.com/macros/s/AKfycbwtc4Gn367FMyA4s3owITC0xagHqbymYWtf-CL_4A6X06PSW33lzehWRV4hy2s5xLg/exec', {
-                            method: 'POST',
-                            headers: { 'Content-Type': 'application/json' },
-                            body: JSON.stringify({
-                                os: os,
-                                af_method: "parsed.pathname.replace",
-                                data: screenName,
-                            }),
-                        })
-                        .then(res => res.text())
-                        .then(console.log)
-                        .catch(console.error);
-                      }
-                      navigation.navigate(screenName, {
-                        referralCodeOnDeeplink,
-                        referralUserIdOnDeeplink,
-                      });
-                    } else {
-                      console.warn('No screen name found in URL:', url);
-                    }
-                } catch (error) {
-                    console.error('Invalid deep link URL:', url, error);
-                }
-             };
-
-
              Linking.getInitialURL().then((url) => {
                if (url) {
                    if (os === 'ios') {
@@ -317,7 +263,26 @@ const LoginScreen: React.FC<{ navigation: any }> = ({ navigation, onLogin }) => 
                    } else {
                      console.log('Running on another platform (e.g., web)');
                    }
-                 handleDeepLink(url);
+                try {
+                    const parsed = new URL(url);
+                    const path = parsed.pathname.replace('/', '');
+                    const screenName = path ? `${path}` : null;
+
+                    const referralCodeOnDeeplink = parsed.searchParams.get('referral_code') || null;
+                    const referralUserIdOnDeeplink = parsed.searchParams.get('af_referrer_customer_id') || null;
+
+                    if (screenName) {
+                      console.log('Navigating to:', screenName);
+                      navigation.navigate(screenName, {
+                        referralCodeOnDeeplink,
+                        referralUserIdOnDeeplink,
+                      });
+                    } else {
+                      console.warn('No screen name found in URL:', url);
+                    }
+                } catch (error) {
+                    console.error('Invalid deep link URL:', url, error);
+                }
                }
              });
 
@@ -353,7 +318,6 @@ const LoginScreen: React.FC<{ navigation: any }> = ({ navigation, onLogin }) => 
                } else {
                  console.log('Running on another platform (e.g., web)');
                }
-               handleDeepLink(event.url);
              });
 
              return () => {
